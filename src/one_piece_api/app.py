cb1be46d1c664d70062039881b7680774b905934
lpereiras <lpereira.qa@gmail.com>
2025-08-08 16:34:48 -0300

    OPA-02(fix):update app.py to be more literal

--- src/one_piece_api/app.py
+++ src/one_piece_api/app.py
@@ -8,14 +8,14 @@ from one_piece_api.schemas.schema_create_new_user import CreateNewUser
 app = FastAPI(version='v0.1.0')
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Version)
+@app.get('/', status_code=200, response_model=Version)
 def API_version():
     return {'version': app.version}
 
 
 @app.post(
     '/one-piece-api/sign-up',
-    status_code=HTTPStatus.CREATED,
+    status_code=201,
     response_model=CreateNewUser,
 )
 def create_new_user():
