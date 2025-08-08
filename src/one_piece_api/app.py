61ae1ce95bbab61f598ae74c9fddd21203987381
lpereiras <lpereira.qa@gmail.com>
2025-08-08 14:59:03 -0300

    OPA-02(fix):general adjusts

--- src/one_piece_api/app.py
+++ src/one_piece_api/app.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
 app = FastAPI(version='v0.1.0')
 
 
-@app.get('/')
-def version_API():
-    return {'version': 'v0.1.0'}
+@app.get('/', status_code=HTTPStatus.OK)
+def API_version():
+    return {'version': app.version}
