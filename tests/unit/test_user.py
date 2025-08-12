1bca36948f32c01b0e6b34c38a09cf19e970c044
lpereiras <lpereira.qa@gmail.com>
2025-08-12 18:23:52 -0300

    OPA-02(fix):configure conftest.py file

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from one_piece_api.app import app
-
-
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/one-piece-api/sign-up',
         json={
