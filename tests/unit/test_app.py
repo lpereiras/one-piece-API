3661406d2acb6127fd3f9efe1f0d7efa2289f3bb
lpereiras <lpereira.qa@gmail.com>
2025-08-13 12:48:44 -0300

    OPA-02(fix):applying conftest concept in my tests

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,12 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from one_piece_api.app import app
-
-
-def test_API_version_validate_version():
-    client = TestClient(app)
+def test_API_version_validate_version(client):
 
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
