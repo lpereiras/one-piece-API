7f6fd25af4a8f096c9e6dc8ee28d7ce7b2f0690a
lpereiras <lpereira.qa@gmail.com>
2025-08-19 18:07:40 -0300

    OPA-03(fix):update test name

--- tests/unit/test_list_users.py
+++ tests/unit/test_list_users.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from one_piece_api.schemas.user_schema import UserPublic
 
 
-def test_list_users(client, test_user):
+def test_list_users_when_exists(client, test_user):
     test_user_schema = UserPublic.model_validate(test_user).model_dump()
 
     response = client.get('/users')
