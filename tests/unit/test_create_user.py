0a7f17f7d0d24aa69aca5afe386a3ef998048962
lpereiras <lpereira.qa@gmail.com>
2025-08-20 20:23:51 -0300

    OPA-03(feat):migration updt/improving test coverage

--- tests/unit/test_create_user.py
+++ tests/unit/test_create_user.py
@@ -37,11 +37,35 @@ def test_create_user_with_existing_username(client):
         '/users/',
         json={
             'username': 'teste_username field',
-            'email': 'teste_email.field@com.br',
+            'email': 'teste_email_valid.field@com.br',
             'password': 'teste_password',
         },
     )
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {
         'detail': 'That username has already been claimed by another pirate.'
+    }
+
+
+def test_create_user_with_existing_email(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'teste_username field',
+            'email': 'teste_email.field@com.br',
+            'password': 'teste_password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'teste_username valid field',
+            'email': 'teste_email.field@com.br',
+            'password': 'teste_password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {
+        'detail': 'This email is already taken.'
     }
